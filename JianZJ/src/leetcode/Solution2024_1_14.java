702ee87d09f595390cefe17caa24684ad967d79b
JianZJ-75 <2279814184@qq.com>
2024-01-14 04:51:16 +0800

    飞书

---
+++ JianZJ/src/leetcode/Solution2024_1_14.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+/**
+ * @Author: JianZJ
+ * @Date: 2024/1/14 0:32
+ */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+public class Solution2024_1_14 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode t = head;
+        while (t != null && t.next != null) {
+            if (t.val == t.next.val)
+                t.next = t.next.next;
+            else
+                t = t.next;
+        }
+        return head;
+    }
+}
