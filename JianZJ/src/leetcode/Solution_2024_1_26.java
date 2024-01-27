199f3693ad031ca3b7c909fd28fb75f3019036fd
JianZJ-75 <2279814184@qq.com>
2024-01-27 17:33:56 +0800

    LeetCode

--- JianZJ/src/leetcode/Solution_2024_1_26.java
+++ JianZJ/src/leetcode/Solution_2024_1_26.java
@@ -1,7 +1,6 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @Author: JianZJ
@@ -12,16 +11,19 @@ public class Solution_2024_1_26 {
     static int n;
     static List<Integer>[] edges = new ArrayList[10000];
     static int[][] fa = new int[10000][21];
-    static int[][] d = new int[10000][10000];
     static int[][] cnt = new int[10000][27];
+    static Map<Integer, Integer>[] mp = new Map[10000];
     public int[] minOperationsQueries(int n, int[][] edges, int[][] queries) {
         this.n = n;
-        init();
+        for (int i = 0; i < n; i++) {
+            mp[i] = new HashMap<>();
+            this.edges[i] = new ArrayList<>();
+        }
         for (int[] i : edges) {
             this.edges[i[0]].add(i[1]);
             this.edges[i[1]].add(i[0]);
-            d[i[0]][i[1]] = i[2];
-            d[i[1]][i[0]] = i[2];
+            mp[i[0]].put(i[1], i[2]);
+            mp[i[1]].put(i[0], i[2]);
         }
         depth[0] = 0;
         dfs(0);
@@ -43,22 +45,15 @@ public class Solution_2024_1_26 {
                     u = fa[u][j];
                     v = fa[v][j];
                 }
+            if (u != v)
+                u = fa[u][0];
             int m = depth[queries[i][0]] + depth[queries[i][1]] - 2 * depth[u];
             int res = m;
             for (int j = 1; j <= 26; j++)
-                res = Math.min(res, m - (cnt[queries[i][0]][j] + cnt[depth[queries[i][1]]][j] - 2 * cnt[u][j]));
+                res = Math.min(res, m - (cnt[queries[i][0]][j] + cnt[queries[i][1]][j] - 2 * cnt[u][j]));
             ans[i] = res;
         }
         return ans;
-    }
-    // 初始化
-    public void init() {
-        for (int i = 0; i < n; i++) {
-            depth[i] = -1;
-            edges[i] = new ArrayList<>();
-            for (int j = 0; j < 15; j++)
-                fa[i][j] = -1;
-        }
     }
     public void dfs(int x) {
         for (int i : edges[x]) {
@@ -66,22 +61,16 @@ public class Solution_2024_1_26 {
                 fa[i][0] = x;
                 for (int j = 1; j <= 26; j++)
                     cnt[i][j] = cnt[fa[i][0]][j];
-                cnt[i][d[i][fa[i][0]]]++;
+                cnt[i][mp[i].get(fa[i][0])]++;
                 depth[i] = depth[fa[i][0]] + 1;
                 dfs(i);
             }
         }
     }
+    // 倍增法
     public void solve() {
         for (int i = 1; i <= 20; i++)
             for (int j = 0; j < n; j++)
                 fa[j][i] = fa[fa[j][i - 1]][i - 1];
-    }
-
-    public static void main(String[] args) {
-        Solution_2024_1_26 s = new Solution_2024_1_26();
-        int[][] a = new int[][]{{0,1,1},{1,2,1},{2,3,1},{3,4,2},{4,5,2},{5,6,2}};
-        int[][] b = new int[][]{{0,3},{3,6},{2,6},{0,6}};
-        System.out.println(s.minOperationsQueries(7, a, b));
     }
 }
