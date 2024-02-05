2e10b27b48d76ccee6598e7eb02a3c806a04d2f6
JianZJ-75 <2279814184@qq.com>
2024-02-05 21:38:10 +0800

    LeetCode

---
+++ JianZJ/src/leetcode/Feb_2024/Solution_2024_2_5.java
@@ -0,0 +1,26 @@
+package leetcode.Feb_2024;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @Author 22798
+ * @Date 2024/2/5 20:51
+ */
+public class Solution_2024_2_5 {
+    public int maxResult(int[] nums, int k) {
+        int n = nums.length;
+        Deque<Integer> q = new ArrayDeque<>();
+        q.add(0);
+        for (int i = 1; i < n; i++) {
+            if (q.peekFirst() < i - k)
+                q.pollFirst();
+            nums[i] += nums[q.peekFirst()];
+            while (!q.isEmpty() && nums[i] >= nums[q.peekLast()]) {
+                q.pollLast();
+            }
+            q.add(i);
+        }
+        return nums[n - 1];
+    }
+}
