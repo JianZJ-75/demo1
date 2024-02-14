904382ad63072750dd42368f7dbedf6d51f47b91
JianZJ-75 <2279814184@qq.com>
2024-02-15 01:02:48 +0800

    LeetCode

--- JianZJ/src/Main.java
+++ JianZJ/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Scanner;
 
 /**
  * @Author: JianZJ
@@ -8,45 +9,9 @@ import java.util.PriorityQueue;
  */
 
 public class Main {
-    public int[] countOfPairs(int n, int x, int y) {
-        int[][] p = new int[n][2];
-        Arrays.sort(p, (a, b) -> a[1] - b[1]);
-        int[] ans = new int[n];
-
-        return ans;
-    }
-    public int minimumTime(List<Integer> nums1, List<Integer> nums2, int x) {
-        int n = nums1.size();
-        int[][] p = new int[n][2];
-        int sum1 = 0, sum2 = 0;
-        for (int i = 0; i < n; i++) {
-            int a = nums1.get(i);
-            int b = nums2.get(i);
-            p[i][0] = a;
-            p[i][1] = b;
-            sum1 += a;
-            sum2 += b;
-        }
-        Arrays.sort(p, (a, b) -> a[1] - b[1]);
-        int[] dp = new int[n + 1];
-        for (int i = 0; i < n; i++)
-            for (int j = i + 1; j > 0; j--)
-                dp[j] = Math.max(dp[j], dp[j - 1] + p[i][0] + j * p[i][1]);
-        for (int i = 0; i <=n ; i++)
-            if (sum1 + sum2 * i - dp[i] <= x)
-                return i;
-        return -1;
-    }
     public static void main(String[] args) {
-        Integer[] array = {4, 2, 8, 1, 6};
-
-        // 使用 Lambda 表达式定义升序排序规则
-        Arrays.sort(array, (a, b) -> a - b);
-
-        // 输出排序后的数组
-        System.out.println(Arrays.toString(array));
-        int t = 1;
-        Integer q = t;
-        System.out.println(q);
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s);
     }
 }
--- JianZJ/src/leetcode/Jan_2024/Solution_2024_1_15.java
+++ JianZJ/src/leetcode/Jan_2024/Solution_2024_1_15.java
@@ -15,6 +15,13 @@ public class Solution_2024_1_15 {
      *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
      * }
      */
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     public ListNode deleteDuplicates(ListNode head) {
         ListNode t = new ListNode();
         t.next = head;
