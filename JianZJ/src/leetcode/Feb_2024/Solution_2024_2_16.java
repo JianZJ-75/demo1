f559dbb134c1134c4bb74ca041a2f81bd7f17743
JianZJ-75 <2279814184@qq.com>
2024-02-17 17:17:29 +0800

    LeetCode

---
+++ JianZJ/src/leetcode/Feb_2024/Solution_2024_2_16.java
@@ -0,0 +1,78 @@
+package leetcode.Feb_2024;
+
+import java.util.*;
+
+/**
+ * @Author 22798
+ * @Date 2024/2/16 16:47
+ */
+public class Solution_2024_2_16 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        bfs(root, ans);
+        int k = 0;
+        for (List<Integer> i : ans)
+        {
+            k++;
+            if (k % 2 == 0)
+                Collections.reverse(i);
+        }
+        return ans;
+    }
+    public void bfs(TreeNode root, List<List<Integer>> ans) {
+        int k = 0, cnt = 1, mx = 1;
+        if (root == null)
+            return;
+        Deque<TreeNode> dq = new ArrayDeque<>();
+        dq.addLast(root);
+        while (cnt != 0)
+        {
+            if (k == 0 && mx == cnt)
+                ans.add(new ArrayList<>());
+            ans.get(ans.size() - 1).add(dq.peekFirst().val);
+            if (dq.peekFirst().left != null) {
+                dq.addLast(dq.peekFirst().left);
+                k++;
+            }
+            if (dq.peekFirst().right != null) {
+                dq.addLast(dq.peekFirst().right);
+                k++;
+            }
+            dq.pollFirst();
+            cnt--;
+            if (cnt == 0)
+            {
+                cnt = k;
+                mx = k;
+                k = 0;
+            }
+        }
+    }
+}
