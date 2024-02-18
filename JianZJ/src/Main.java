5421bb09b329d4e764d44398acf10b1b95f1d5a1
JianZJ-75 <2279814184@qq.com>
2024-02-18 17:48:03 +0800

    LeetCode

--- JianZJ/src/Main.java
+++ JianZJ/src/Main.java
@@ -1,7 +1,4 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @Author: JianZJ
@@ -10,8 +7,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        System.out.println(s);
+        String q = "Dada";
+        q = new StringBuilder(q).reverse().toString();
+        System.out.println(new Main().lastNonEmptyString("aabcbbca"));
+    }
+    public String lastNonEmptyString(String s) {
+        String res = new String();
+        Map<Character, List<Integer>> mp = new HashMap<>();
+        int n = s.length();
+        int mx = 0;
+        for (int i = 0; i < n; i++) {
+            char j = s.charAt(i);
+            mp.computeIfAbsent(j, t -> new ArrayList<>()).add(i);
+            mp.putIfAbsent(j, new ArrayList<>());
+            mp.get(j).add(i);
+            mx = Math.max(mx, mp.get(j).size());
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (List<Integer> i : mp.values()) {
+            if (i.size() == mx)
+                ans.add(i.get(mx - 1));
+        }
+        ans.sort((a, b) -> a - b);
+        for (int i : ans)
+            res += s.charAt(i);
+        return res;
     }
 }
