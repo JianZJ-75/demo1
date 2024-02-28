54cc0710482611f54e38f11a5cd536c292974b51
JianZJ-75 <2279814184@qq.com>
2024-02-28 19:47:59 +0800

    LeetCode

---
+++ JianZJ/src/leetcode/Feb_2024/Solution_2024_2_28.java
@@ -0,0 +1,16 @@
+package leetcode.Feb_2024;
+
+/**
+ * @Author 22798
+ * @Date 2024/2/28 19:38
+ */
+public class Solution_2024_2_28 {
+    public int minIncrements(int n, int[] cost) {
+        int res = 0;
+        for (int i = n / 2; i >= 1; i--) {
+            res += Math.abs(cost[2 * i - 1] - cost[2 * i]);
+            cost[i - 1] += Math.max(cost[2 * i - 1], cost[2 * i]);
+        }
+        return res;
+    }
+}
