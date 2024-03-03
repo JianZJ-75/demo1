d1665f09b984fa3ceefda249ae9477216bf7eb89
JianZJ-75 <2279814184@qq.com>
2024-03-03 17:44:06 +0800

    LeetCode

---
+++ JianZJ/src/test/Virus01.java
@@ -0,0 +1,15 @@
+package test;
+
+import javax.swing.*;
+
+/**
+ * @Author JianZJ
+ * @Date 2024/3/2 19:19
+ */
+public class Virus01 {
+    public static void main(String[] args) {
+        while (true) {
+            JOptionPane.showMessageDialog(null, "hcc就是答辩！！！！");
+        }
+    }
+}
