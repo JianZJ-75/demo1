80c41cd4b42887f710ed9eedb1b951503684f739
JianZJ-75 <2279814184@qq.com>
2024-03-04 19:32:49 +0800

    LeetCode

---
+++ JianZJ/src/leetcode/Mar_2024/Solution_2024_3_4.java
@@ -0,0 +1,56 @@
+package leetcode.Mar_2024;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @Author JianZJ
+ * @Date 2024/3/4 19:30
+ */
+public class Solution_2024_3_4 {
+    class MyQueue {
+        Deque<Integer> inStack;
+        Deque<Integer> outStack;
+
+        public MyQueue() {
+            inStack = new ArrayDeque<Integer>();
+            outStack = new ArrayDeque<Integer>();
+        }
+
+        public void push(int x) {
+            inStack.push(x);
+        }
+
+        public int pop() {
+            if (outStack.isEmpty()) {
+                in2out();
+            }
+            return outStack.pop();
+        }
+
+        public int peek() {
+            if (outStack.isEmpty()) {
+                in2out();
+            }
+            return outStack.peek();
+        }
+
+        public boolean empty() {
+            return inStack.isEmpty() && outStack.isEmpty();
+        }
+
+        private void in2out() {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+    }
+    /**
+     * Your MyQueue object will be instantiated and called as such:
+     * MyQueue obj = new MyQueue();
+     * obj.push(x);
+     * int param_2 = obj.pop();
+     * int param_3 = obj.peek();
+     * boolean param_4 = obj.empty();
+     */
+}
