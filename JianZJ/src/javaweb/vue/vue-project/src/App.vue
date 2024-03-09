d3e528af51010e7cf5abfa635ab960ab3c2176d6
JianZJ-75 <2279814184@qq.com>
2024-03-10 01:38:49 +0800

    vue

--- JianZJ/src/javaweb/vue/vue-project/src/App.vue
+++ JianZJ/src/javaweb/vue/vue-project/src/App.vue
@@ -1,13 +1,16 @@
 <!-- 模板部分, 由它生成HTML代码 -->
 <template>
   <div>
-    <h1>{{ message }}</h1>
+    <!-- <h1>{{ message }}</h1> -->
+    <ElementView></ElementView>
   </div>
 </template>
 
 <!-- 控制模板的数据来源和行为 -->
 <script>
+import ElementView from './views/element/ElementView.vue'
 export default {
+  components: { ElementView },
   data() {
     return {
       message: "Hello Vue"
