3ffb2b036c5780ca111a0ddc5dbbb8ede06999b1
JianZJ-75 <2279814184@qq.com>
2024-03-10 00:13:23 +0800

    javaweb

--- JianZJ/src/leetcode/Mar_2024/Solution_2024_3_9.java
+++ JianZJ/src/leetcode/Mar_2024/Solution_2024_3_9.java
@@ -1,14 +1,41 @@
 package leetcode.Mar_2024;
 
+import java.util.Arrays;
+
 /**
  * @Author JianZJ
- * @Date 2024/3/8 15:25
+ * @Date 2024/3/9 17:42
  */
 public class Solution_2024_3_9 {
-    static final int MOD = 1_000_000_007;
-    public int minimumPossibleSum(int n, int target) {
-        long m = Math.min(n, target / 2);
-//        注意下 ！一定要把target*2放在(n-m-1)的后面 否则当target*2和n特别大时可能会超出限制～
-        return (int) (((m + 1) * m + (n - m - 1 + target + target) * (n - m)) / 2 % MOD);
+    long cnt;
+    public long kSum(int[] nums, int k) {
+        long sum = 0;
+        long l = -1, r = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0)
+                sum += nums[i];
+            else
+                nums[i] *= -1;
+            r += nums[i];
+        }
+        Arrays.sort(nums);
+        while (r - l > 1) {
+            long mid = (l + r) / 2;
+            cnt = k - 1;
+            dfs(0, mid, nums);
+            if (cnt == 0)
+                r = mid;
+            else
+                l = mid;
+        }
+        return sum - r;
+    }
+
+    public void dfs(int i, long sum, int[] nums) {
+        if (cnt == 0 || i == nums.length || sum - nums[i] < 0)
+            return;
+        cnt--;
+        dfs(i + 1, sum - nums[i], nums);
+        dfs(i + 1, sum, nums);
     }
 }
