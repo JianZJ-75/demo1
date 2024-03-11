01f235cc9443dc15fe4c823063ab547beaa1c684
JianZJ-75 <2279814184@qq.com>
2024-03-11 21:54:34 +0800

    javaweb

--- JianZJ/src/javaweb/vue/vue-project/src/views/element/ElementView.vue
+++ JianZJ/src/javaweb/vue/vue-project/src/views/element/ElementView.vue
@@ -25,11 +25,84 @@
         <br>
 
         <!-- Pagination 分页组件 -->
-        <el-pagination background layout="total, ->, sizes, prev, pager, next, jumper" 
-         @size-change="handleSizeChange"
-         @current-change="handleCurrentChange" 
-         :total="1000">
+        <el-pagination background layout="total, ->, sizes, prev, pager, next, jumper" @size-change="handleSizeChange"
+            @current-change="handleCurrentChange" :total="1000">
         </el-pagination>
+
+        <br><br>
+
+        <!-- Dialog对话框 -->
+        <!-- Table -->
+        <el-button type="text" @click="dialogTableVisible = true">打开嵌套表格的 Dialog</el-button>
+
+        <el-dialog title="收货地址" :visible.sync="dialogTableVisible">
+            <el-table :data="gridData">
+                <el-table-column property="date" label="日期" width="150"></el-table-column>
+                <el-table-column property="name" label="姓名" width="200"></el-table-column>
+                <el-table-column property="address" label="地址"></el-table-column>
+            </el-table>
+        </el-dialog>
+
+        &nbsp;&nbsp;&nbsp;&nbsp;
+
+        <!-- Form -->
+        <el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>
+
+        <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+            <el-form :model="form">
+                <el-form-item label="活动名称" :label-width="formLabelWidth">
+                    <el-input v-model="form.name" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="活动区域" :label-width="formLabelWidth">
+                    <el-select v-model="form.region" placeholder="请选择活动区域">
+                        <el-option label="区域一" value="shanghai"></el-option>
+                        <el-option label="区域二" value="beijing"></el-option>
+                    </el-select>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+            </div>
+        </el-dialog>
+
+        <br><br>
+
+        <!-- 弹出表单 -->
+        <el-row>
+            <el-button type="primary" @click="formVisible = true">添加</el-button>
+            <el-dialog title="个人信息" :visible.sync="formVisible">
+                <el-form ref="form" :model="form1" label-width="80px">
+                    <el-form-item label="活动名称">
+                        <el-input v-model="form1.name"></el-input>
+                    </el-form-item>
+
+                    <el-form-item label="活动区域">
+                        <el-select v-model="form1.region" placeholder="请选择活动区域">
+                            <el-option label="区域一" value="shanghai"></el-option>
+                            <el-option label="区域二" value="beijing"></el-option>
+                        </el-select>
+                    </el-form-item>
+
+                    <el-form-item label="活动时间">
+                        <el-col :span="11">
+                            <el-date-picker type="date" placeholder="选择日期" v-model="form1.date1"
+                                style="width: 100%;"></el-date-picker>
+                        </el-col>
+                        <el-col class="line" :span="2">-</el-col>
+                        <el-col :span="11">
+                            <el-time-picker placeholder="选择时间" v-model="form1.date2"
+                                style="width: 100%;"></el-time-picker>
+                        </el-col>
+                    </el-form-item>
+                    
+                    <el-form-item>
+                        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+                        <el-button>取消</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-dialog>
+        </el-row>
     </div>
 </template>
 
@@ -37,6 +110,50 @@
 export default {
     data() {
         return {
+            gridData: [{
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }],
+            dialogTableVisible: false,
+
+            dialogFormVisible: false,
+            form: {
+                name: '',
+                region: '',
+                date1: '',
+                date2: '',
+                delivery: false,
+                type: [],
+                resource: '',
+                desc: ''
+            },
+            formLabelWidth: '120px',
+
+            form1: {
+                name: '',
+                region: '',
+                date1: '',
+                date2: '',
+                delivery: false,
+                type: [],
+                resource: '',
+                desc: ''
+            },
+            formVisible: false,
+
             tableData: [{
                 date: '2004-xx-xx',
                 name: '侯陈琛',
@@ -49,11 +166,16 @@ export default {
         }
     },
     methods: {
-        handleSizeChange: (val) => {
+        handleSizeChange(val) {
             alert("每页记录数变化" + val);
         },
-        handleCurrentChange: (val) => {
+        handleCurrentChange(val) {
             alert("页码发生变化" + val);
+        },
+
+        onSubmit() {
+            alert(JSON.stringify(this.form1));
+            console.log('submit!');
         }
     }
 }
