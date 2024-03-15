efb6517676ff3ffb73a8eec375da45bd2c2a0212
JianZJ-75 <2279814184@qq.com>
2024-03-16 01:38:38 +0800

    Maven

---
+++ JavaWeb/itheima_web_project/maven-projecte01/src/test/java/com/itheima/DemoTest.java
@@ -0,0 +1,24 @@
+package com.itheima;
+
+import org.junit.Test;
+
+/**
+ * @Author JianZJ
+ * @Date 2024/3/16 1:09
+ */
+public class DemoTest {
+    @Test
+    public void test1() {
+        System.out.println("Hello Test1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Hello Test2");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("Hello Test3");
+    }
+}
