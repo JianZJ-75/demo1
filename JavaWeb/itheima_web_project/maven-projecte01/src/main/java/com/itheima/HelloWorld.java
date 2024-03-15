f400416f82fa58ce66d6befb356458fc28dc0b6b
JianZJ-75 <2279814184@qq.com>
2024-03-16 00:27:53 +0800

    Maven

---
+++ JavaWeb/itheima_web_project/maven-projecte01/src/main/java/com/itheima/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+/**
+ * @Author JianZJ
+ * @Date 2024/3/16 0:19
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven");
+    }
+}
