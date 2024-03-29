63918efe397c41059dbb78f17a47c0d1da6c6f2f
JianZJ-75 <2279814184@qq.com>
2024-03-29 21:12:53 +0800

    LeetCode

---
+++ JianZJ/src/leetcode/Mar_2024/Solution_2024_3_29.java
@@ -0,0 +1,26 @@
+package leetcode.Mar_2024;
+
+import java.util.Arrays;
+
+/**
+ * @Author JianZJ
+ * @Date 2024/3/29 20:52
+ */
+public class Solution_2024_3_29 {
+    public int minimumSum(int[] nums) {
+        int n = nums.length;
+        int[] pre = new int[n];
+        int[] last = new int[n];
+        Arrays.setAll(pre, i -> Integer.MAX_VALUE / 2);
+        Arrays.setAll(last, i -> Integer.MAX_VALUE / 2);
+        for (int i = 1; i < n - 1; i++)
+            pre[i] = Math.min(pre[i - 1], nums[i - 1]);
+        for (int i = n - 2; i > 0; i--)
+            last[i] = Math.min(last[i + 1], nums[i + 1]);
+        int ans = Integer.MAX_VALUE / 2;
+        for (int i = 1; i < n - 1; i++)
+            if (pre[i] < nums[i] && last[i] < nums[i])
+                ans = Math.min(ans, pre[i] + nums[i] + last[i]);
+        return ans == Integer.MAX_VALUE / 2 ? -1 : ans;
+    }
+}
