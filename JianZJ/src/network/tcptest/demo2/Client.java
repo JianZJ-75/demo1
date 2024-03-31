f9d5fbc8e2f98d1cff66355aee46c1efa7a17348
JianZJ-75 <2279814184@qq.com>
2024-03-31 12:26:39 +0800

    LeetCode

---
+++ JianZJ/src/network/tcptest/demo2/Client.java
@@ -0,0 +1,15 @@
+package network.tcptest.demo2;
+
+/**
+ * @Author JianZJ
+ * @Date 2024/3/31 0:29
+ */
+
+/*
+* TCP通信练习2 --- 接收和反馈
+* 客户端：发送一条数据，接收服务器反馈的消息并打印
+* 服务端：接收数据并打印，再给客户反馈消息
+* */
+
+public class Client {
+}
