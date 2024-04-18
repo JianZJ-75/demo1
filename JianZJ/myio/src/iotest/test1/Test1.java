f1f8f4ee8c66a8c976cd12b54be61bb6575c93c5
JianZJ-75 <2279814184@qq.com>
2024-04-19 00:27:45 +0800

    IO

--- JianZJ/myio/src/iotest/test1/Test1.java
+++ JianZJ/myio/src/iotest/test1/Test1.java
@@ -5,8 +5,7 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.regex.Matcher;
@@ -70,6 +69,56 @@ public class Test1 {
             }
         }
 
+        // 5. 生成数据
+        // 姓名(唯一) - 性别 - 年龄
+        List<String> list = getInfos(familyNameList, boyNameList, girlNameList, 70, 50);
+        Collections.shuffle(list);
+        System.out.println(list);
+
+    }
+
+    /*
+    * 作用:
+    *       获取男生和女生的信息: 张三-男-23
+    * 形参:
+    *       参数一: 装着姓氏的集合
+    *       参数二: 装着男生名字的集合
+    *       参数三: 装着女生名字的集合
+    *       参数四: 男生的个数
+    *       参数五: 女生的个数
+    * */
+    private static List<String> getInfos(List<String> familyNameList, List<String> boyNameList, List<String> girlNameList, int boyCount, int girlCount) {
+        // 1. 生成男生不重复的名字
+        Set<String> boyName = new HashSet<>();
+        while (boyName.size() != boyCount) {
+            // 随机
+            Collections.shuffle(familyNameList);
+            Collections.shuffle(boyNameList);
+            boyName.add(familyNameList.get(0) + boyNameList.get(0));
+        }
+        // 2. 生成女生不重复的明细
+        Set<String> girlName = new HashSet<>();
+        while (girlName.size() != girlCount) {
+            // 随机
+            Collections.shuffle(familyNameList);
+            Collections.shuffle(girlNameList);
+            girlName.add(familyNameList.get(0) + girlNameList.get(0));
+        }
+        // 3. 生成男生的信息并添加到集合当中
+        List<String> list = new ArrayList<>();
+        Random r = new Random();
+        // [18 ~ 27]
+        for (String boy : boyName) {
+            int age = r.nextInt(10) + 18;
+            list.add(boy + "-男-" + age);
+        }
+        // 4. 生成女生的信息并添加到集合当中
+        // [18 ~ 25]
+        for (String girl : girlName) {
+            int age = r.nextInt(8) + 18;
+            list.add(girl + "-女-" + age);
+        }
+        return list;
     }
 
     /*
