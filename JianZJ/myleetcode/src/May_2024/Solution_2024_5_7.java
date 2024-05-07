7ddf1d8de10877e979de249b0a8330d3e4c3120d
JianZJ-75 <2279814184@qq.com>
2024-05-07 22:42:29 +0800

    LeetCode

---
+++ JianZJ/myleetcode/src/May_2024/Solution_2024_5_7.java
@@ -0,0 +1,32 @@
+package May_2024;
+
+/**
+ * @Author JianZJ
+ * @Date 2024/5/7 21:47
+ */
+public class Solution_2024_5_7 {
+    public int cherryPickup(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][][] f = new int[m + 1][n + 2][n + 2];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 0; j < Math.min(n, i + 1); j++) {
+                for (int k = Math.max(j + 1, n - 1 - i); k < n; k++) {
+                    f[i][j + 1][k + 1] = max(
+                            f[i + 1][j][k], f[i + 1][j][k + 1], f[i + 1][j][k + 2],
+                            f[i + 1][j + 1][k], f[i + 1][j + 1][k + 1], f[i + 1][j + 1][k + 2],
+                            f[i + 1][j + 2][k], f[i + 1][j + 2][k + 1], f[i + 1][j + 2][k + 2]
+                    ) + grid[i][j] + grid[i][k];
+                }
+            }
+        }
+        return f[0][1][n];
+    }
+
+    private int max(int x, int... y) {
+        for (int v : y) {
+            x = Math.max(x, v);
+        }
+        return x;
+    }
+}
