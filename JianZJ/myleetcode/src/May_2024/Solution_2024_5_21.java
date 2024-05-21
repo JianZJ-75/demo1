cde2ccc7c9b8226969c1262a9e55c33311ab56d2
JianZJ-75 <2279814184@qq.com>
2024-05-21 23:02:20 +0800

    LeetCode

---
+++ JianZJ/myleetcode/src/May_2024/Solution_2024_5_21.java
@@ -0,0 +1,11 @@
+package May_2024;
+
+/**
+ * @Author JianZJ
+ * @Date 2024/5/21 22:53
+ */
+public class Solution_2024_5_21 {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + 2 * t;
+    }
+}
