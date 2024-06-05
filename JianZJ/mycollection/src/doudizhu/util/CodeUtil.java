d9c3125185ccf8d1992291cac7d626f758db24be
JianZJ <2279814184@qq.com>
2024-06-05 20:39:47 +0800

    doudizhu

--- JianZJ/mycollection/src/doudizhu/util/CodeUtil.java
+++ JianZJ/mycollection/src/doudizhu/util/CodeUtil.java
@@ -1,12 +1,33 @@
 package doudizhu.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class CodeUtil {
 
+    static List<Character> list = new ArrayList<>();
+
+    static {
+        for (int i = 0; i < 26; i++) {
+            list.add((char)('a' + i));
+            list.add((char)('A' + i));
+        }
+    }
+
     public static String getCode(){
-      //自己练习
-        return "a";
+        List<Character> tmp = new ArrayList<>();
+        Random r = new Random();
+        for (int i = 0; i < 4; i++)
+            tmp.add(list.get(r.nextInt(list.size())));
+        tmp.add((char)r.nextInt(10));
+        tmp.add((char)r.nextInt(10));
+        Collections.shuffle(tmp);
+        String ans = "";
+        for (Character i : tmp) {
+            ans += i;
+        }
+        return ans;
     }
 }
