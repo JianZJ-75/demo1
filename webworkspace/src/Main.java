f5bad75c3b247c14cd4530be2670fd21783864be
JianZJ-75 <2279814184@qq.com>
2024-06-12 23:11:23 +0800

    web

--- webworkspace/src/Main.java
+++ webworkspace/src/Main.java
@@ -1,5 +1,11 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        FileWriter fw = new FileWriter(new File("src\\resource\\servlet\\tmp.txt"));
+        fw.write("zj");
+        fw.close();
     }
 }
---
+++ webworkspace/src/servlet/Servlet03.java
@@ -0,0 +1,44 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.*;
+
+/**
+ * @Author JianZJ
+ * @Date 2024/6/12 20:05
+ */
+@WebServlet(name = "Servlet03", urlPatterns = "/test")
+public class Servlet03 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        String name = request.getParameter("name");
+//        if (name != null) {
+//            FileWriter fw = new FileWriter(new File("webworkspace\\src\\resource\\servlet\\tmp.txt"));
+//            fw.write("zj");
+//            fw.close();
+//        }
+        File file = new File("a.txt");
+        FileWriter fw = new FileWriter(file);
+        fw.write(name);
+        fw.close();
+        FileReader fr = new FileReader(file);
+        char[] chars = new char[2];
+        int len;
+        String res = "";
+        while ((len = fr.read(chars)) != -1) {
+            res += new String(chars, 0, len);
+        }
+        pw.println(res + " " + file.getAbsolutePath() + " " + file.getPath());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
