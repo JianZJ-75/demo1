a6b4aabefeaaaf8f9927faabd27eaf36e91847c7
JianZJ-75 <2279814184@qq.com>
2024-06-14 19:08:28 +0800

    Study

--- JianZJ/mythread/src/test/practice1/test3/MyThread.java
+++ JianZJ/mythread/src/test/practice1/test3/MyThread.java
@@ -13,6 +13,8 @@ public class MyThread extends Thread{
     static List<Integer> prizePool;
 
     List<Integer> now = new ArrayList<>();
+    int max = 0;
+    int sum = 0;
 
     public MyThread(List<Integer> prizePool) {
         this.prizePool = prizePool;
@@ -28,8 +30,20 @@ public class MyThread extends Thread{
                     Collections.shuffle(prizePool);
                     int prize = prizePool.remove(0);
                     now.add(prize);
+                    max = Math.max(max, prize);
+                    sum += prize;
                 }
             }
+        }
+        synchronized (MyThread.class) {
+            System.out.println("在此次抽奖过程中, " + getName() + "总共产生了" + now.size() + "个奖项");
+            System.out.print("分别为: ");
+            for (int i = 0; i < now.size(); i++) {
+                System.out.print(now.get(i));
+                if (i != now.size() - 1)
+                    System.out.print(",");
+            }
+            System.out.println("最高奖项为" + max + "元, 总计额为" + sum + "元");
         }
     }
 }
