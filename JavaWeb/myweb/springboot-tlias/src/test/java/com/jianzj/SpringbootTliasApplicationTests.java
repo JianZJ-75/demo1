2d5c91c2825f3ee8cdacd5338eaf1365edeb5d83
JianZJ-75 <2279814184@qq.com>
2024-07-08 01:18:19 +0800

    springboot

---
+++ JavaWeb/myweb/springboot-tlias/src/test/java/com/jianzj/SpringbootTliasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jianzj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTliasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
