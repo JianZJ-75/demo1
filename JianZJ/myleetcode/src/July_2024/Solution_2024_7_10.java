6fd01932e7e146569bef1b16efe55e42cb8462b5
JianZJ-75 <2279814184@qq.com>
2024-07-10 04:51:07 +0800

    LeetCode

--- JianZJ/myleetcode/src/July_2024/Solution_2024_7_10.java
+++ JianZJ/myleetcode/src/July_2024/Solution_2024_7_10.java
@@ -5,5 +5,20 @@ package July_2024;
  * @Date 2024/7/10 1:26
  */
 public class Solution_2024_7_10 {
-
+    public int incremovableSubarrayCount(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        while (i + 1 < n && nums[i + 1] > nums[i]) {
+            i++;
+        }
+        if (i == n - 1)
+            return n * (n + 1) / 2;
+        int ans = i + 2;
+        for (int j = n - 1; j == n - 1 || nums[j] < nums[j + 1]; j--) {
+            while (i >= 0 && nums[i] >= nums[j])
+                i--;
+            ans += i + 2;
+        }
+        return ans;
+    }
 }
