9244629499d7c589321fb524265cab2e505c6a07
JianZJ-75 <2279814184@qq.com>
2024-07-11 03:44:23 +0800

    springboot

--- JavaWeb/myweb/springboot-tlias/src/main/java/com/jianzj/filter/DemoFilter.java
+++ JavaWeb/myweb/springboot-tlias/src/main/java/com/jianzj/filter/DemoFilter.java
@@ -12,18 +12,22 @@ import java.io.IOException;
 
 @WebFilter("/*")
 public class DemoFilter implements Filter {
+
     @Override // 初始化方法, 只调用一次
-    public void destroy() {
+    public void init(FilterConfig filterConfig) throws ServletException {
         System.out.println("init 初始化方法执行了");
     }
 
     @Override // 拦截到请求之后调用, 调用多次
-    public void init(FilterConfig filterConfig) throws ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         System.out.println("拦截到了请求");
+        // 放行
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
-    @Override // 销毁方法, 只调用一次
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    @Override // 销毁方法, 只调用一次// 初始化方法, 只调用一次
+    public void destroy() {
         System.out.println("destroy 销毁方法执行了");
     }
+
 }
