cec832f54fcbb175efaf942ac1bcb73d658aa850
JianZJ-75 <2279814184@qq.com>
2024-07-14 00:51:23 +0800

    LeetCode

---
+++ JianZJ/myleetcode/src/July_2024/Solution_2024_7_14.java
@@ -0,0 +1,26 @@
+package July_2024;
+
+/**
+ * @Author JianZJ
+ * @Date 2024/7/14 0:44
+ */
+public class Solution_2024_7_14 {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int n = grid.length;
+        int[] row = new int[n];
+        int[] col = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                row[i] = Math.max(row[i], grid[i][j]);
+                col[j] = Math.max(col[j], grid[i][j]);
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans += Math.min(row[i], col[j]) - grid[i][j];
+            }
+        }
+        return ans;
+    }
+}
