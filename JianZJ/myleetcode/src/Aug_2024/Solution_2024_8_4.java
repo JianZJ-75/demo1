76a5dea5bbfdfb2f8cedeaeab497f62e8bdecf75
JianZJ-75 <2279814184@qq.com>
2024-08-04 22:40:27 +0800

    LeetCode

---
+++ JianZJ/myleetcode/src/Aug_2024/Solution_2024_8_4.java
@@ -0,0 +1,62 @@
+package Aug_2024;
+
+/**
+ * @Author JianZJ
+ * @Date 2024/8/4 22:39
+ */
+public class Solution_2024_8_4 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+            if (root == null) {
+                return false;
+            }
+            return isSameTree(root, subRoot) ||
+                    isSubtree(root.left, subRoot) ||
+                    isSubtree(root.right, subRoot);
+        }
+
+        // 100. 相同的树
+        private boolean isSameTree(TreeNode p, TreeNode q) {
+            if (p == null || q == null) {
+                return p == q; // 必须都是 null
+            }
+            return p.val == q.val &&
+                    isSameTree(p.left, q.left) &&
+                    isSameTree(p.right, q.right);
+        }
+    }
+}
