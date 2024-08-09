f777bd5c45c73aff5af1345cb67fa0424de515cc
JianZJ-75 <2279814184@qq.com>
2024-08-09 22:03:21 +0800

    LeetCode

--- JianZJ/myleetcode/src/Aug_2024/Solution_2024_8_9.java
+++ JianZJ/myleetcode/src/Aug_2024/Solution_2024_8_9.java
@@ -10,6 +10,15 @@ public class Solution_2024_8_9 {
     public int minimumAddedInteger(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        
+        for (int i = 2; i > 0; i--) {
+            int x = nums2[0] - nums1[i];
+            int j = 0;
+            for (int k = i; k < nums1.length; k++) {
+                if (nums1[k] + x == nums2[j] && ++j == nums2.length) {
+                    return x;
+                }
+            }
+        }
+        return nums2[0] - nums1[0];
     }
 }
