af880a882f01f2b4f64d1a85ec74c653f9091978
JianZJ-75 <2279814184@qq.com>
2024-09-11 20:40:50 +0800

    LeetCode

---
+++ JianZJ/myleetcode/src/Sept_2024/Solution_2024_9_11.java
@@ -0,0 +1,26 @@
+package Sept_2024;
+
+/**
+ * @Author JianZJ
+ * @Date 2024/9/11 20:11
+ */
+public class Solution_2024_9_11 {
+    // TODO
+    public int maximizeWin(int[] prizePositions, int k) {
+        int n = prizePositions.length;
+        if (k * 2 + 1 >= prizePositions[n - 1] - prizePositions[0]) {
+            return n;
+        }
+        int ans = 0;
+        int left = 0;
+        int[] mx = new int[n + 1];
+        for (int right = 0; right < n; right++) {
+            while (prizePositions[right] - prizePositions[left] > k) {
+                left++;
+            }
+            ans = Math.max(ans, mx[left] + right - left + 1);
+            mx[right + 1] = Math.max(mx[right], right - left + 1);
+        }
+        return ans;
+    }
+}
