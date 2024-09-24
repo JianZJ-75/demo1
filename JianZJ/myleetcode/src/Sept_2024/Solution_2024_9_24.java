0a6208ba398b41436ee544ad7fc25516a5e3ae59
JianZJ-75 <2279814184@qq.com>
2024-09-24 22:13:37 +0800

    LeetCode

--- JianZJ/myleetcode/src/Sept_2024/Solution_2024_9_24.java
+++ JianZJ/myleetcode/src/Sept_2024/Solution_2024_9_24.java
@@ -5,19 +5,18 @@ package Sept_2024;
  * @Date 2024/9/24 21:41
  */
 public class Solution_2024_9_24 {
-    // TODO
     public long maximumSubsequenceCount(String text, String pattern) {
+        long ans = 0;
         char x = pattern.charAt(0);
         char y = pattern.charAt(1);
-        long ans = 0;
-        int cntX = 0;
-        int cntY = 0;
-        for (char c : text.toCharArray()) {
-            if (c == y) {
+        int cntX = 0, cntY = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char t = text.charAt(i);
+            if (t == y) {
                 ans += cntX;
                 cntY++;
             }
-            if (c == x) {
+            if (t == x) {
                 cntX++;
             }
         }
