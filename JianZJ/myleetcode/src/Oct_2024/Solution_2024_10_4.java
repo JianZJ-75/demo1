011f2bc68f38d8b850bceb1feb96ded08f6058b4
JianZJ-75 <2279814184@qq.com>
2024-10-04 18:16:16 +0800

    LeetCode

---
+++ JianZJ/myleetcode/src/Oct_2024/Solution_2024_10_4.java
@@ -0,0 +1,11 @@
+package Oct_2024;
+
+/**
+ * @Author JianZJ
+ * @Date 2024/10/4 18:11
+ */
+public class Solution_2024_10_4 {
+    public double nthPersonGetsNthSeat(int n) {
+        return n == 1 ? 1 : 0.5;
+    }
+}
