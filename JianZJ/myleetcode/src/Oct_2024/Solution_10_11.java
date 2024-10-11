b760fa9562f73f50d8f40dbcf5fd258c429e15da
JianZJ-75 <2279814184@qq.com>
2024-10-11 21:20:48 +0800

    LeetCode

---
+++ JianZJ/myleetcode/src/Oct_2024/Solution_10_11.java
@@ -0,0 +1,33 @@
+package Oct_2024;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author JianZJ
+ * @Date 2024/10/11 21:20
+ */
+public class Solution_10_11 {
+    public long numberOfPairs(int[] nums1, int[] nums2, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int x : nums1) {
+            if (x % k != 0) {
+                continue;
+            }
+            x /= k;
+            for (int i = 1; i * i <= x; i++) {
+                if (x % i == 0) {
+                    map.put(i, map.getOrDefault(i, 0) + 1);
+                    if (i != x / i) {
+                        map.put(x / i, map.getOrDefault(x / i, 0) + 1);
+                    }
+                }
+            }
+        }
+        long ans = 0;
+        for (int x : nums2) {
+            ans += map.getOrDefault(x, 0);
+        }
+        return ans;
+    }
+}
