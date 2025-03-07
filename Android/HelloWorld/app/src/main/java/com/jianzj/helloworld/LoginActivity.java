e0a2b840da9e19df0bc8ab4d42ce318b368fde11
JianZJ-75 <2279814184@qq.com>
2025-03-07 17:00:40 +0800

    andriod

--- Android/HelloWorld/app/src/main/java/com/jianzj/helloworld/LoginActivity.java
+++ Android/HelloWorld/app/src/main/java/com/jianzj/helloworld/LoginActivity.java
@@ -9,23 +9,25 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText userName;
+
+    private EditText username;
     private EditText password;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        userName = findViewById(R.id.edit_user);
-        password = findViewById(R.id.edit_password);
-        Button getContentButton = findViewById(R.id.get_content_button);
+        username = findViewById(R.id.account_input);
+        password = findViewById(R.id.password_input);
+        Button getContentButton = findViewById(R.id.login_button);
         // 设置按钮点击事件监听器
         getContentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 获取输入框中的内容
-                String userContent = userName.getText().toString();
+                String usernameContent = username.getText().toString();
                 String passwordContent = password.getText().toString();
-                String content = "用户名：" + userContent + "\n密码：" + passwordContent;
+                String content = "用户名: " + usernameContent + "\n密码: " + passwordContent;
                 // 显示获取到的内容
                 Toast.makeText(LoginActivity.this, "输入的内容是: " + content, Toast.LENGTH_SHORT).show();
             }
